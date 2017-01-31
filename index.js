60a604a6918d62f3b791ffffa91fff20c28c3247
pkanuma <pkanumala@walmartlabs.com>
2017-01-30 23:20:49 -0800

    updating call to sams club locator

--- index.js
+++ index.js
@@ -32,50 +32,79 @@ router.get("/", function(req, res) {
 
 router.post("/clubdetails", function(req, res) {
 	console.log('this is the request: '+ JSON.stringify(req.body));
-    var clublocatorrequest = {
-    	postaladdress: req.body.postaladdress,
-    	radius: '50',
-    	offset: '0',
-    	limit: '10'
-    };
-    
-    var url = 'https://www.samsclub.com/soa/services/v1/clublocator/address?'+querystring.stringify(clublocatorrequest);
-    options.url = url;
-    console.log('url: '+ url);
-    httprequest.get(options, function (error, response, body) {
-  		if (!error && response.statusCode == 200) {
-    		console.log(body) // Show the HTML for the Google homepage. 
-    		var clublocatorpayload = JSON.parse(body); 		
-			res.set('Content-Type', 'application/json');  
-			var txt = '';
-			if(clublocatorpayload.payload.clubs.length === 1) {
-				txt = 'There is ';
-			} else {
-				txt = 'There are ';
-			}
-			var speechtxt = txt + clublocatorpayload.payload.clubs.length+ ' clubs near by'+req.body.postaladdress + 
-				' and the nearest club is '+ clublocatorpayload.payload.clubs[0].clubName + ' at '+ clublocatorpayload.payload.clubs[0].streetAddress+','
-				+clublocatorpayload.payload.clubs[0].city;
 
-			var responsepayload = {
-    			speech: speechtxt,
-    			displayText: speechtxt,
-    			data: clublocatorpayload,
-    			source: 'testapi'
-    		};		
-			res.send(JSON.stringify(responsepayload));
-  		} else {
-			res.set('Content-Type', 'application/json');   
-  			var speechtxt = 'There are no clubs found near by';
-  			var responsepayload = {
-    			speech: speechtxt,
-    			displayText: speechtxt,
-    			data: '',
-    			source: 'testapi'
-    		};
-    		res.send(JSON.stringify(responsepayload));
-  		}
-	});
+    var parameters =  req.body.result.parameters;
+    var location;
+    var postaladdress;
+
+    if(Object.prototype.hasOwnProperty.call(parameters, 'location')){
+    	location = parameters.location;
+    	if(Object.prototype.hasOwnProperty.call(location, 'geo-city-us') && 
+    		Object.prototype.hasOwnProperty.call(location, 'geo-state-us')){
+    		postaladdress = location['geo-city-us']+' '+location['geo-state-us'];
+    	} else if(Object.prototype.hasOwnProperty.call(location, 'zip-code')){
+    		postaladdress = location['zip-code'];
+    	}
+    }
+
+    if(postaladdress === undefined){
+    	res.set('Content-Type', 'application/json');   
+		var speechtxt = 'There are no clubs found near by';
+		var responsepayload = {
+			speech: speechtxt,
+			displayText: speechtxt,
+			data: '',
+			source: 'testapi'
+		};
+		res.send(JSON.stringify(responsepayload));
+    } else {
+    	var clublocatorrequest = {
+	    	postaladdress: postaladdress,
+	    	radius: '50',
+	    	offset: '0',
+	    	limit: '10'
+	    };
+    	var url = 'https://www.samsclub.com/soa/services/v1/clublocator/address?'+querystring.stringify(clublocatorrequest);
+	    options.url = url;
+	    console.log('url: '+ url);
+	    httprequest.get(options, function (error, response, body) {
+	  		if (!error && response.statusCode == 200) {
+	    		console.log(body) // Show the HTML for the Google homepage. 
+	    		var clublocatorpayload = JSON.parse(body); 		
+				res.set('Content-Type', 'application/json');  
+				var txt = '';
+				if(clublocatorpayload.payload.clubs.length === 1) {
+					txt = 'There is ';
+				} else {
+					txt = 'There are ';
+				}
+				var speechtxt = txt + clublocatorpayload.payload.clubs.length+ ' clubs near by'+req.body.postaladdress + 
+					' and the nearest club is '+ clublocatorpayload.payload.clubs[0].clubName + ' at '
+					+ clublocatorpayload.payload.clubs[0].address.streetAddress+' '
+					+clublocatorpayload.payload.clubs[0].address.city + ' '+ clublocatorpayload.payload.clubs[0].address.state;
+
+				var responsepayload = {
+	    			speech: speechtxt,
+	    			displayText: speechtxt,
+	    			data: clublocatorpayload,
+	    			source: 'testapi'
+	    		};		
+				res.send(JSON.stringify(responsepayload));
+	  		} else {
+				res.set('Content-Type', 'application/json');   
+	  			var speechtxt = 'There are no clubs found near by';
+	  			var responsepayload = {
+	    			speech: speechtxt,
+	    			displayText: speechtxt,
+	    			data: '',
+	    			source: 'testapi'
+	    		};
+	    		res.send(JSON.stringify(responsepayload));
+	  		}
+		});
+    }
+    
+
  });
  
 var server = app.listen(3000, function () {
